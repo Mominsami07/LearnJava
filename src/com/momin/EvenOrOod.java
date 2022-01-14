b0072edf641b2613481d2683c853d6489021fa4a
mominsami07 <momensami009@gmail.com>
2022-01-14 01:33:18 +0100

    Check if number is even or ood

---
+++ src/com/momin/EvenOrOod.java
@@ -0,0 +1,18 @@
+package com.momin;
+import java.util.Scanner;
+
+public class EvenOrOod {
+
+    public static void main(String[] args) {
+        // write your code here
+        Scanner input=new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int number =input.nextInt();
+
+        if(number%2==0)
+            System.out.println(number +" is Even.");
+        else
+            System.out.println(number+" is Odd.");
+    }
+}
